9a0a48bc6a5d70cc54a0695a7a03fdc790976821
Raymond Blum <raygeeknyc@gmail.com>
2019-02-24 22:00:54 +0000

    Add usage instruction

--- startup.sh
+++ startup.sh
@@ -16,6 +16,8 @@ done
 MY_ADDRESS=$(ifconfig | grep "inet " | grep -v "127.0.0.1" | awk '{print $2}')
 echo "My address is $MY_ADDRESS"
 ./showtext "My address is $MY_ADDRESS"
+sleep 2
+./showtext 'quit quit quit to exit'
 
 python ./example_recognizer.py 2>/tmp/piday.err >/tmp/piday.out
 sleep 2
