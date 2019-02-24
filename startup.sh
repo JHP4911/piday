8d2014c3c57e3afc70554d4afa312a48f9e8fd41
Raymond Blum <raygeeknyc@gmail.com>
2019-02-24 04:23:54 +0000

    Added connectivity test and display of IP address

--- startup.sh
+++ startup.sh
@@ -3,4 +3,18 @@ cmd_dir=`dirname $0`
 cd $cmd_dir
 export GOOGLE_APPLICATION_CREDENTIALS=`ls ../*json | head -1`
 echo $GOOGLE_APPLICATION_CREDENTIALS
+./showtext 'waiting for IP'
+while :
+do
+  sleep 1
+  ping -c 1 www.github.com 2>&1
+  rc=$?
+  if [[ $rc -eq 0 ]]; then
+    break
+  fi
+done
+MY_ADDRESS=$(ifconfig | grep "inet " | grep -v "127.0.0.1" | awk '{print $2}')
+echo "My address is $MY_ADDRESS"
+./showtext "My address is $MY_ADDRESS"
+
 python ./example_recognizer.py
